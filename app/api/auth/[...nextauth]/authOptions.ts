6a523826d8bb920db8bfe5ed497da497aeff6488
shmulik <shmulik.reihani@tikalk.com>
2023-07-31 16:32:40 +0300

    auth option pages

--- app/api/auth/[...nextauth]/authOptions.ts
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -9,5 +9,8 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
     })
   ],
+  pages: {
+    signIn: '/signIn',
+  },
   secret: process.env.AUTH_SECRET,
 }
